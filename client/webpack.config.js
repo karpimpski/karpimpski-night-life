3bbe734485689552b79f89170af662aa3a8f547c
Michael Karpinski <mike.karpinski99@gmail.com>
2017-02-10 23:53:12 -0500

    more optimization

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,3 +1,5 @@
+const webpack = require('webpack');
+
 module.exports = {
 	entry: './src/index.js',
 	output: {
@@ -18,5 +20,16 @@ module.exports = {
         loader: 'style-loader!css-loader!sass-loader'
 			}
 		]
-	}
+	},
+	plugins: [
+	    new webpack.DefinePlugin({
+	      // A common mistake is not stringifying the "production" string.
+	      'process.env.NODE_ENV': JSON.stringify('production')
+	    }),
+	    new webpack.optimize.UglifyJsPlugin({
+	      compress: {
+	        warnings: false
+	      }
+	    })
+	  ]
 }
